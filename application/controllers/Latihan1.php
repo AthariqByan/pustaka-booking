3d16ee76678427d1c24c1066caa0042e537f89f5
AthariqByan <fadhlansantoso25@gmail.com>
2022-09-27 20:42:59 +0700

    Merubah

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -6,12 +6,26 @@ class Latihan1 extends CI_Controller{
      public function __construct()
      {
           parent::__construct();
+
           //Codeigniter : Write Less Do More
      }
 
-     function index()
+     public function index()
      {
-          echo "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo explicabo deleniti corporis expedita veritatis deserunt, reprehenderit nulla, ab commodi consequuntur porro excepturi odio consectetur ut beatae, blanditiis sint mollitia quia?";
+          echo "Lorem ipsum dolorem amet";
      }
 
+     public function penjumlahan ($n1, $n2)
+     {
+          $this->load->model('Model_latihan1');
+
+          $data['nilai1'] = $n1;
+          $data['nilai2'] = $n2;
+          $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
+
+          $this->load->view('view-latihan', $data);
+
+          //$hasil = $this->Model_latihan1->jumlah($n1, $n2);
+          //echo "Hasil penjumlahan dari ". $n1 ." + ". $n2 ." = " .$hasil;
+     }
 }
