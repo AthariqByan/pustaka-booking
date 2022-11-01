e1542327b8a66644e0577355fcc6bc87347a5251
AthariqByan <fadhlansantoso25@gmail.com>
2022-11-01 17:48:08 +0700

    tambahan

--- application/views/siswa/index.php
+++ application/views/siswa/index.php
@@ -9,7 +9,7 @@
             </div>
             <?php }?>
             <?= $this->session->flashdata('pesan'); ?>
-            <a href="siswa/tambah" type="submit" class="btn btn-primary">Siswa Baru</a>
+            <a href="siswa/tambah" type="submit" class="btn btn-primary mb-3"><i class="fas fa-file-alt"></i> Siswa Baru</a>
             <table class="table table-hover">
                 <thead>
                     <tr>
